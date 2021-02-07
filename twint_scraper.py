f49fefef60f8938dedb9f4667b0fa1be93146e86
Adithya-Shankar <54188170+shanksy25@users.noreply.github.com>
2021-02-07 02:20:51 -0500

    Update twint_scraper.py

--- twint_scraper.py
+++ twint_scraper.py
@@ -1,12 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 6 10:03:47 2021
+@author: adi
+"""
+
 import nest_asyncio
 nest_asyncio.apply()
 
 import twint
 
 c = twint.Config()
-#c.Username = "KarenBot17"
 c.Lang = "en"
-
 
 c.Search = ["plandemic" or "masksoffamerica" or "pandemichoax"]
 c.Limit = 600
@@ -14,5 +18,4 @@ c.Since = "2020-10-06 13:00:00"
 c.Store_csv = True
 c.Output = "none.csv"
 
-# Run
 twint.run.Search(c)
